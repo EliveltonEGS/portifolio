a6eec3755a821a7a4fab1f49f16d8be3e88166dd
Elivelton <eliveltongsoficial@gmail.com>
2023-07-17 00:45:43 -0300

    refatoring project

---
+++ assets/anime.js
@@ -0,0 +1,31 @@
+anime({
+    targets: ".header",
+    opacity: [0, 1],
+    duration: 1000,
+    delay: 1000,
+    easing: "easeInOutSine",
+});
+
+anime({
+    targets: ".top",
+    opacity: [0, 1],
+    duration: 1500,
+    delay: 1500,
+    easing: "easeInOutSine",
+});
+
+anime({
+    targets: ".text-title",
+    opacity: [0, 1],
+    duration: 1000,
+    delay: 1000,
+    easing: "easeInOutSine",
+});
+
+anime({
+    targets: ".all-tec",
+    opacity: [0, 1],
+    duration: 1500,
+    delay: 1500,
+    easing: "easeInOutSine",
+});
